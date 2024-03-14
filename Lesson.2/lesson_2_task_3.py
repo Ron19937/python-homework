b41b40d192ed91e8619c41100f12532347edb92e
Евгений Тищенко <153735798+Ron19937@users.noreply.github.com>
2024-03-14 16:58:44 +0700

    add second HW

---
+++ Lesson.2/lesson_2_task_3.py
@@ -0,0 +1,8 @@
+def square(n):
+	s = n*n
+	
+	n = (s)
+	
+	return s
+	
+print(square(5.01))
---
+++ Lesson.2/lesson_2_task_5.py
@@ -0,0 +1,13 @@
+def month_to_season(a):
+    if (a == 1) or (a == 2) or (a == 12):
+        return("зима")
+    elif (a == 3) or(a == 4) or (a == 5):
+        return("весна")
+    elif (a == 6) or (a == 7) or (a == 8):
+        return("лето")
+    elif (a == 9) or (a == 10) or (a == 11):
+        return("осень")
+    elif (a == 0) or (a == 13):
+        return (None)
+    
+print (month_to_season(int(input("Введите номер месяца"))))
